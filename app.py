e06d864a8718c5f9e12aa30e032ccece5a57acc5
paul michael <mopaul@student.lautech.edu.ng>
2023-10-31 11:37:18 +0100

    certification mvp

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        if not request.form.get("name") or not request.form.get("email"):
+            return ("Please fill input approprately")
+        return render_template("certificate.html", name=request.form.get("name", "world"))
